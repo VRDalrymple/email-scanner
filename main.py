20911410df116081f40ccaacb883ccf9c02c9c62
V.R. Dalrymple <142173019+VRDalrymple@users.noreply.github.com>
2026-03-30 15:51:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
 import sys
 import os
-import json
 import win32com.client as win32
 from enum import Enum
-import pandas as pd
-import cv2
-from PIL import Image
-import pytesseract
-import platform
-
-def scan(image):
-	if platform.system() == "Windows":
-    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-	print(image)
-	text = ''
-	image = str(image)
-
-	if image.endswith('.jpg') or image.endswith('.jpeg') or image.endswith('.png'):
-			image = cv2.imread(image)
-			try:
-					image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-					_, threshold_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-					image = cv2.fastNlMeansDenoising(threshold_image, None, 30, 7, 21)
-			except Exception as err:
-					print(f'Error: {err}')
-			try:
-					custom_config = r'--oem 3 --psm 3'
-					text_data = pytesseract.image_to_string(image, config=custom_config)
-					text += text_data
-			except Exception as err:
-					print(f'Error: {err}')
-
-	return text
-
+from ocr import scan
 
 class OutlookFolder(Enum):
     olFolderDeletedItems = 3 # The Deleted Items folder
@@ -42,42 +12,21 @@ class OutlookFolder(Enum):
     olFolderDrafts = 16 # The Drafts folder
     olFolderJunk = 23 # The Junk E-Mail folder
 
-with open("vardata.json", "r") as f:
-	vardata = json.load(f)
-  
 # Get a reference to Outlook
 outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
 # Get the Inbox folder
 inbox = outlook.GetDefaultFolder(OutlookFolder.olFolderInbox.value)
 
-# Get subfolder
-try:
-	if vardata["folder"] != "":
-		folder = inbox.Folders.Item(f"{vardata["folder"]}")
-	else:
-		messages = inbox.Items
-except Exception as err:
-  print(f"Error: {err}"}
-	print("Subfolder not found. Scanning from main inbox.")
-	messages = inbox.Items
+# Get subfolder "Reps"
+reps = inbox.Folders.Item("Reps")
 
-# Apply restrictions
-try:
-	for restriction in vardata["restrictions"]:
-		try:
-		  if vardata["restrictions"][restriction] != "":
-		    messages = folder.Items.Restrict(f"{vardata["restrictions"][restriction]}")
-		except Exception as err:
-		  print(f"Error: {err}"}
-		  print("Restriction failed.")
-except Exception as err:
-	  print(f"Error: {err}"}
-	  print("Restrictions failed.")
+# Apply the restriction
+messages = reps.Items.Restrict("[ReceivedTime]>='2025-05-01'")
 
 # Check if messages exist
 if len(messages) == 0:
-    print("There aren't any messages to scan in this folder")
+    print("There aren't any unread messages in this folder")
     exit()
 
 # Initialize text file
@@ -85,12 +34,13 @@ output_file = "OutreachResults.txt"
 with open(output_file, "w", encoding="utf-8") as file:
     # Loop over messages
     for message in messages:
-        sender = message.SenderName.upper()
+        name = message.SenderName.upper()
         date_sent = message.Senton.date()
         email_body = message.Body
         attachments = message.Attachments
+        print(name)
         
-        file.write(f"Name: {sender}\nDate Sent: {date_sent}\n\nEmail Content:\n{email_body}\n")
+        file.write(f"Name: {name}\nDate Sent: {date_sent}\n\nEmail Content:\n{email_body}\n\n")
         
         if len(attachments) > 0:
             for attachment in attachments:
@@ -101,6 +51,8 @@ with open(output_file, "w", encoding="utf-8") as file:
                 if scanned:
                     file.write("Attachment Content:\n")
                     file.write(scanned + "\n\n")  # Directly write the full text
+
+
         
-        file.write("-_" * 100 + "-\n\n")
-        print(f"Processed unread email from {sender}")
+        file.write("-" * 100 + "\n\n")
+        print(f"Processed unread email from {repname}")
